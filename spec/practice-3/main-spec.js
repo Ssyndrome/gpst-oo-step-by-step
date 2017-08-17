3c63e96529e3e6514eb1d7a23f8ed948c9be4903
915448715@qq.com <915448715@qq.com>
2017-08-17 10:31:27 +0800

    transfer spyOn to equal

--- spec/practice-3/main-spec.js
+++ spec/practice-3/main-spec.js
@@ -156,9 +156,8 @@ describe("Class", () => {
     let clazz = new Class(2);
     let student = new Student("Jerry", 21, clazz);
     let teacher = new Teacher("Tom", 21, [clazz]);
-    spyOn(teacher, 'notifyStudentAppended');
     clazz.appendMember(student);
-    expect(teacher.notifyStudentAppended).toHaveBeenCalledWith("Jerry has joined Class 2");
+    expect(teacher.notifyStudentAppended()).toEqual("Jerry has joined Class 2");
   });
 
   it("should call teacher's assign class leader event, when a student is assigned to b" +
@@ -167,11 +166,9 @@ describe("Class", () => {
     let clazz = new Class(2);
     let student = new Student("Jerry", 21, clazz);
     let teacher = new Teacher("Tom", 21, [clazz]);
-    spyOn(teacher, 'notifyLeaderAssigned');
-    spyOn(teacher, 'notifyStudentAppended');
     clazz.appendMember(student);
     clazz.assignLeader(student);
-    expect(teacher.notifyLeaderAssigned).toHaveBeenCalledWith("Jerry become Leader of Class 2")
+    expect(teacher.notifyLeaderAssigned()).toEqual("Jerry become Leader of Class 2");
   });
 
 });
